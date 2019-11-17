fe8e7095910009aab5ec92b635acdbbdbf047f05
孙路 <>
2019-11-17 15:48:15 +0800

    add buildconfig

---
+++ build-config/config.js
@@ -0,0 +1,17 @@
+exports.mysql = {
+    connectionLimit: 2,
+    host: 'localhost',
+    user: 'dev',
+    password: '12345Qwert_',
+    database: 'sn_user'
+};
+
+exports.redis = {
+    port: 6379,
+    host: "127.0.0.1",
+    // This is the default value of `retryStrategy`
+    retryStrategy: function (times) {
+        const delay = Math.min(times * 50, 2000);
+        return delay;
+    }
+};
